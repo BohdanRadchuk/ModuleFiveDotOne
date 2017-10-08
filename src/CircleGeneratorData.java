fd5bae7d18120d60651deacb901f63b50a172a99
BohdanRadchuk <shpionuse@gmail.com>
2017-10-08 13:07:37 +0300

    circle optimization

--- src/CircleGeneratorData.java
+++ src/CircleGeneratorData.java
@@ -1,31 +1,24 @@
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 
 import java.util.Random;
 import java.util.Scanner;
 
-public class CircleGenerator {
-/*
-    Random rand = new Random();
-    public void randomColor () {
-        float r = rand.nextFloat();
-        float g = rand.nextFloat();
-        float b = rand.nextFloat();
+public class CircleGeneratorData {
 
-    }
-*/
-
-
-    static int x;
+    static int x;                                               //x=static
     int y;
     int circlearraysize;
-    int randomeradius;
+
     int Min;
     int Max;
+
     Scanner scanner = new Scanner(System.in);
 
-    public CircleGenerator (){
+
+    public CircleGeneratorData(){                                  //setting our data from keyboard
         System.out.println("enter amount of circles");
         this.circlearraysize = scanner.nextInt();
         System.out.println("enter minimal circle radius");
@@ -35,5 +28,12 @@ public class CircleGenerator {
 
         this.x = Max;
     }
+
+    private static  Random r = new Random();
+
+    public static Color getRandom() {                                               //creating color randomization
+            return Color.color(r.nextDouble(), r.nextDouble(), r.nextDouble());
+        }
+
 
 }
--- src/Fxmain.java
+++ src/Fxmain.java
@@ -3,10 +3,9 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
-import java.util.Scanner;
+import java.util.Random;
 
 public class Fxmain extends Application {
 
@@ -16,38 +15,40 @@ public class Fxmain extends Application {
         launch(args);
     }
     public void initCircles(Pane root) {
-        CircleGenerator gen = new CircleGenerator();
+        CircleGeneratorData gen = new CircleGeneratorData();
 
-        Line line = new Line(10, 10, 200, 200);
+
         Circle[] circle = new Circle[gen.circlearraysize];
-        int[] randomeradius = new int[gen.circlearraysize];
-        //CircleGenerator randomColor = new CircleGenerator();
+        int[] randomeradius = new int[gen.circlearraysize];                                         //creating array with size from keyboard
 
+
+        Random rand = new Random();
+        int sumofrad = 0;
         for (int i = 0; i < gen.circlearraysize; i++) {
-            randomeradius[i] = gen.Min + (int) (Math.random() * ((gen.Max - gen.Min) + 1));}
-
-            for (int i = 0; i < gen.circlearraysize; i++) {
-            if (i==0){
-                gen.y = randomeradius[i];
-                Circle circle1 = new Circle(randomeradius[i]/2 , randomeradius[i]/2,randomeradius[i]/4, Paint.valueOf("Green"));
-                root.getChildren().addAll(circle1);
-            }
-            else {
-                gen.y = gen.y + randomeradius[i] + randomeradius[i-1];
-            }
-
-            circle[i] = new Circle(gen.x, gen.y, randomeradius[i]);
-            circle[i].setStroke(Paint.valueOf(""));
-            circle[i].setFill (Paint.valueOf("Green"));
-                root.getChildren().addAll(circle[i]);
-            }
-
-
-
-
+            randomeradius[i] = gen.Min + (int) (Math.random() * ((gen.Max - gen.Min) + 1));         //creating array with random radiuses of our circles
+            sumofrad = sumofrad + randomeradius[i] * 2 + 1;                                         //getting snowmen total length
         }
 
+        for (int i = gen.circlearraysize; i > 0; i--) {                                             //drawing snowman
+            gen.y = sumofrad - randomeradius[i - 1];
+            sumofrad = sumofrad - randomeradius[i - 1] * 2 - 1;
 
+            circle[i - 1] = new Circle(gen.x, gen.y, randomeradius[i - 1]);
+            circle[i - 1].setStroke(gen.getRandom());                                               //drawing random color stroke
+            circle[i - 1].setFill(Paint.valueOf("white"));
+            root.getChildren().addAll(circle[i - 1]);
+        }
+
+        Circle[] head = new Circle[]{new Circle(gen.x - randomeradius[0] / 2.5, randomeradius[0] / 2, randomeradius[0] / 8),        //drawing 2 eyes and nose
+                new Circle(gen.x + randomeradius[0] / 2.5, randomeradius[0] / 2, randomeradius[0] / 6),
+                new Circle( gen.x, randomeradius[0] * 5 / 4, randomeradius[0] / 5)};
+        for (int i = 0; i < head.length; i++) {
+            head[i].setFill(Paint.valueOf("white"));
+            head[i].setStroke(gen.getRandom());
+            root.getChildren().addAll(head[i]);
+        }
+
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
