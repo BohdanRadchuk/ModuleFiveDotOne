ab69596369e96a8dd9f46b9990d29cef6a230650
BohdanRadchuk <shpionuse@gmail.com>
2017-10-09 14:24:08 +0300

    wrong cos

--- src/Task2/StarData.java
+++ src/Task2/StarData.java
@@ -1,4 +1,72 @@
 package Task2;
 
+import java.awt.*;
+import java.util.Scanner;
+
+import static java.lang.Math.PI;
+import static java.lang.Math.sin;
+import static java.lang.StrictMath.cos;
+
 public class StarData {
+    int radius = 100;
+    int centerx = 100;
+    int centery = 100;
+    double smallradius;
+    double [] stardotsx = new double[10];
+    double [] stardotsy = new double[10];
+
+
+    public StarData() {
+        /*System.out.println("enter x & y of the star center");
+        Scanner scanner= new Scanner(System.in);
+        this.centerx = scanner.nextInt();
+        this.centery = scanner.nextInt();
+        System.out.println("enter radius of the star");
+        this.radius = scanner.nextInt();
+        */this.smallradius = radius/2.57;
+
+    }
+
+
+    public void pointsInit(){
+
+
+
+        stardotsx [0] =  centerx;
+        stardotsy [0] = centery - radius;
+        for (int i = 1; i <10;i ++) {
+            if (i % 2 == 0) {
+                stardotsx[i] = centerx - cos((90 + 72 * i) * (PI / 180)) * radius;
+                stardotsy[i] = centery - sin((90 + 72 * i) * (PI / 180)) * radius;
+            } else {
+                stardotsx[i] = centerx - cos((90 + 36 * i) * (PI / 180)) * smallradius;
+                stardotsy[i] = centery - sin((90 + 36 * i) * (PI / 180)) * smallradius;
+            }
+        }
+        /*stardotsx [1] = centerx + cos(18 * (PI / 180) ) * radius;
+        stardotsy [1] = centery - sin( 18 * (PI / 180)) * radius;
+        stardotsx [2] = centerx + sin (36* (PI / 180) ) * radius;
+        stardotsy [2] = centery + cos(36 * (PI / 180)) * radius;
+        stardotsx [3] = centerx - sin(36 * (PI / 180)) * radius;
+        stardotsy [3] = centery  + cos(36 * (PI / 180)) * radius;
+        stardotsx [4] = centery - cos(18 * (PI / 180)) * radius;
+        stardotsy [4] = centery - sin(18 * (PI / 180)) * radius;
+
+
+
+
+        stardotsx [5] = centerx - cos(36 * (PI/180)) *smallradius;
+        stardotsy [5] = centery + sin(36 *(PI/180)) *smallradius;
+        stardotsx [6] = centerx + cos(36 * (PI/180)) *smallradius;
+        stardotsy [6] = centery + sin(36 *(PI/180)) *smallradius;
+        stardotsx [7] = centerx;
+        stardotsy [7] = centery + smallradius;
+*/
+
+
+
+    }
+    public void  show(){
+        System.out.println("point0" + stardotsx[1]);
+    }
 }
--- src/Task2/StarMain.java
+++ src/Task2/StarMain.java
@@ -1,6 +1,9 @@
 package Task2;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 public class StarMain extends Application {
@@ -9,8 +12,39 @@ public class StarMain extends Application {
         launch(args);
     }
 
+
+    public void initStar(Pane root){
+        StarData sd = new StarData();
+        sd.pointsInit();
+        /*for (int i = 1; i<10; i++) {
+            Line line = new Line(sd.stardotsx[i - 1], sd.stardotsy[i - 1], sd.stardotsx[i], sd.stardotsy[i]);
+            root.getChildren().addAll(line);
+        }
+*/
+            Line lineend = new Line(sd.stardotsx[0], sd.stardotsy[0], sd.stardotsx[1], sd.stardotsy[1]);
+        Line lineend1 = new Line(sd.stardotsx[1], sd.stardotsy[1], sd.stardotsx[6], sd.stardotsy[6]);
+        Line lineend2 = new Line(sd.stardotsx[6], sd.stardotsy[6], sd.stardotsx[3], sd.stardotsy[3]);
+        Line lineend3 = new Line(sd.stardotsx[3], sd.stardotsy[3], sd.stardotsx[2], sd.stardotsy[2]);
+        Line lineend4 = new Line(sd.stardotsx[2], sd.stardotsy[2], sd.stardotsx[5], sd.stardotsy[5]);
+        Line lineend5 = new Line(sd.stardotsx[5], sd.stardotsy[5], sd.stardotsx[8], sd.stardotsy[8]);
+
+        root.getChildren().addAll(lineend,lineend1,lineend2,lineend3,lineend4,lineend5);
+
+
+        sd.show();
+
+
+    }
+
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception {
+
+
+        Pane root = new Pane();
+        initStar(root);
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
     }
 }
