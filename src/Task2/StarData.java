d776984c0e0a7108e8e2988524c5d3ad7632a67d
BohdanRadchuk <shpionuse@gmail.com>
2017-10-09 16:54:53 +0300

    ready to draw a star

--- src/Task2/StarData.java
+++ src/Task2/StarData.java
@@ -8,22 +8,24 @@ import static java.lang.Math.sin;
 import static java.lang.StrictMath.cos;
 
 public class StarData {
-    int radius = 100;
-    int centerx = 100;
-    int centery = 100;
+    int radius;
+    int centerx;
+    int centery;
     double smallradius;
     double [] stardotsx = new double[10];
     double [] stardotsy = new double[10];
 
 
     public StarData() {
-        /*System.out.println("enter x & y of the star center");
+        System.out.println("enter x & y of the star center");
         Scanner scanner= new Scanner(System.in);
         this.centerx = scanner.nextInt();
         this.centery = scanner.nextInt();
         System.out.println("enter radius of the star");
         this.radius = scanner.nextInt();
-        */this.smallradius = radius/2.57;
+        this.smallradius = radius/2.57;
+        double tempx;
+        double tempy;
 
     }
 
@@ -34,39 +36,34 @@ public class StarData {
 
         stardotsx [0] =  centerx;
         stardotsy [0] = centery - radius;
-        for (int i = 1; i <10;i ++) {
-            if (i % 2 == 0) {
+        for (int i = 1; i <10;i ++) {                                   //creating 10 points
+            if (i % 2 == 0) {                                           // creating 5 points of bigradius
                 stardotsx[i] = centerx - cos((90 + 72 * i) * (PI / 180)) * radius;
                 stardotsy[i] = centery - sin((90 + 72 * i) * (PI / 180)) * radius;
-            } else {
+            } else {                                                                            //creating 5 points with swamm radius
                 stardotsx[i] = centerx - cos((90 + 36 * i) * (PI / 180)) * smallradius;
                 stardotsy[i] = centery - sin((90 + 36 * i) * (PI / 180)) * smallradius;
             }
         }
-        /*stardotsx [1] = centerx + cos(18 * (PI / 180) ) * radius;
-        stardotsy [1] = centery - sin( 18 * (PI / 180)) * radius;
-        stardotsx [2] = centerx + sin (36* (PI / 180) ) * radius;
-        stardotsy [2] = centery + cos(36 * (PI / 180)) * radius;
-        stardotsx [3] = centerx - sin(36 * (PI / 180)) * radius;
-        stardotsy [3] = centery  + cos(36 * (PI / 180)) * radius;
-        stardotsx [4] = centery - cos(18 * (PI / 180)) * radius;
-        stardotsy [4] = centery - sin(18 * (PI / 180)) * radius;
+        double tempx = stardotsx [2];               //swaping points position
+        double tempy = stardotsy [2];
+        stardotsx [2] = stardotsx [6];
+        stardotsy [2] = stardotsy [6];
 
+        double tempx1 = stardotsx [4];
+        double tempy1 = stardotsy [4];
+        stardotsx [4] = tempx;
+        stardotsy [4] = tempy;
+        tempx = stardotsx [8];
+        tempy = stardotsy [8];
+        stardotsx [8] = tempx1;
+        stardotsy [8] = tempy1;
+        stardotsx [6] = tempx;
+        stardotsy [6] = tempy;
 
-
-
-        stardotsx [5] = centerx - cos(36 * (PI/180)) *smallradius;
-        stardotsy [5] = centery + sin(36 *(PI/180)) *smallradius;
-        stardotsx [6] = centerx + cos(36 * (PI/180)) *smallradius;
-        stardotsy [6] = centery + sin(36 *(PI/180)) *smallradius;
-        stardotsx [7] = centerx;
-        stardotsy [7] = centery + smallradius;
-*/
 
 
 
     }
-    public void  show(){
-        System.out.println("point0" + stardotsx[1]);
-    }
+
 }
--- src/Task2/StarMain.java
+++ src/Task2/StarMain.java
@@ -16,22 +16,16 @@ public class StarMain extends Application {
     public void initStar(Pane root){
         StarData sd = new StarData();
         sd.pointsInit();
-        /*for (int i = 1; i<10; i++) {
-            Line line = new Line(sd.stardotsx[i - 1], sd.stardotsy[i - 1], sd.stardotsx[i], sd.stardotsy[i]);
+        for (int i = 1; i<10; i++) {
+            Line line = new Line(sd.stardotsx[i - 1], sd.stardotsy[i - 1], sd.stardotsx[i], sd.stardotsy[i]);       //drawing 9 lines
             root.getChildren().addAll(line);
         }
-*/
-            Line lineend = new Line(sd.stardotsx[0], sd.stardotsy[0], sd.stardotsx[1], sd.stardotsy[1]);
-        Line lineend1 = new Line(sd.stardotsx[1], sd.stardotsy[1], sd.stardotsx[6], sd.stardotsy[6]);
-        Line lineend2 = new Line(sd.stardotsx[6], sd.stardotsy[6], sd.stardotsx[3], sd.stardotsy[3]);
-        Line lineend3 = new Line(sd.stardotsx[3], sd.stardotsy[3], sd.stardotsx[2], sd.stardotsy[2]);
-        Line lineend4 = new Line(sd.stardotsx[2], sd.stardotsy[2], sd.stardotsx[5], sd.stardotsy[5]);
-        Line lineend5 = new Line(sd.stardotsx[5], sd.stardotsy[5], sd.stardotsx[8], sd.stardotsy[8]);
 
-        root.getChildren().addAll(lineend,lineend1,lineend2,lineend3,lineend4,lineend5);
+        Line lineend = new Line(sd.stardotsx[0], sd.stardotsy[0], sd.stardotsx[9], sd.stardotsy[9]);                //drawing finishing line
+
+        root.getChildren().addAll(lineend);
 
 
-        sd.show();
 
 
     }
